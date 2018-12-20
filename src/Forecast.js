10b616364343bb3295ba000e86b4dac42d99f4b3
Stanley Chen <dannyshien@gmail.com>
2018-12-19 21:57:54 -0500

    finally able to show something for forecast!

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const Forecast = (props) => {
-    const showForecast = props.threeHourForecast.map((props) => { console.log(`HERE IS YO PROPS:`, props)
+    const showForecast = props.threeHourForecast.map((natalie) => { 
+        let forecastTemp = ((natalie.forecast_weather[0].main.temp - 273.15) * 9/5 +32).toFixed(1);
+        // console.log(`HERE IS YO PROPS:`, natalie.forecast_weather[0].main.temp)
         return ( 
             <div className = 'forecast'>
                 <h2>3 hr Interval Forecast</h2>
-                    <div>{`ehh, props.showForecast`}</div>
+                    <div>{forecastTemp}</div>
+                    <div>{forecastTemp}</div>
+                    <div>{forecastTemp}</div>
+                    
             </div>  
         )       
     })
