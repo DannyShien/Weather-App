5907980c5e595f73d9200b4a4b8d36dea07e08bc
Stanley Chen <dannyshien@gmail.com>
2019-07-27 14:31:08 -0400

    Edit some styling for Navbar and App.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
+
 // Functional componenet that just returns JSX for a navbar.
 const Navbar = () => {
     return (
-        <div className = 'navbar sticky'>
+        <div className = 'navbar'>
             {/* Using 'Link' instead of an achor tag keep the links within React.  */}
             <Link to = '/' className = 'links'>Home</Link>
+            <Search />
         </div>
     );
 };
