9d3df8a78e719010cc335bcd0d4008c0a4d15af7
Stanley Chen <dannyshien@gmail.com>
2021-01-28 18:30:50 -0500

    Remove components for a fresh start

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // Import BrowserRouter to use Route
-import {
-    BrowserRouter as Router,
-    Route,
+// import {
+//     BrowserRouter as Router,
+//     Route,
 
-}   from 'react-router-dom';
-import './App.css';
+// }   from 'react-router-dom';
+// import './App.css';
 
 // Importing other components to connect my files.
-import Home from './components/Home';
-import Weather from './components/Weather';
-
+// import Home from './components/Home';
+// import Weather from './components/Weather';
 
 class App extends Component {
-    render() {
-        return (
-            <Router>
-                <div className="App">
-                    <Route path = '/' exact component = {Home} />
-                    <Route path = '/weather' component = {Weather} /> 
-                </div>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          {/* <Route path = '/' exact component = {Home} /> */}
+          {/* <Route path = '/weather' component = {Weather} />  */}
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
